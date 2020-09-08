16959471e3a92e5a131ecdd17d279fb98e166ec7
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 12:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign = ({},object,{[key]:value});
+  return Object.assign({},object,{[key]:value});
+   return Object.assign({}, obj, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
