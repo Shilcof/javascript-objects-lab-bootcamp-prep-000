c46f3c8b4c95fdb71e55b36375a3ce95015af820
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 11:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign = ({},{object},{[key]: value});
 }
 
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+
+ function deleteFromObjectByKey(object, key) {
+   
+ }
